fa89df69eb0f95499d1f60ecd34dcdc4b15dd89b
sherryxiaoyizhu <zhuxy9474@gmail.com>
2020-10-18 23:08:42 -0500

    Update count functions

--- app/src/main/java/com/example/kotlinyoutube/OneVideoAdapter.kt
+++ app/src/main/java/com/example/kotlinyoutube/OneVideoAdapter.kt
@@ -9,8 +9,20 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinyoutube.OneVideoActivity.Companion.videoURL
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.row_one_video.view.*
+import java.text.NumberFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class OneVideoAdapter(private val onePlaylist: OnePlaylist): RecyclerView.Adapter<OneVideoViewHolder>() {
+
+    companion object {
+        private const val SECOND_MILLIS = 1000
+        private const val MINUTE_MILLIS = 60 * Companion.SECOND_MILLIS
+        private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+        private const val DAY_MILLIS = 24 * HOUR_MILLIS
+    }
 
     override fun getItemCount(): Int {
         return 1
@@ -28,14 +40,77 @@ class OneVideoAdapter(private val onePlaylist: OnePlaylist): RecyclerView.Adapte
 
         // display video detail thumbnail
         val thumbnailImageView = holder.view.videoDetailImageView
-        Picasso.with(holder.view.context).load(oneVideo.snippet.thumbnails.standard.url).into(thumbnailImageView)
+        Picasso.with(holder.view.context).load(oneVideo.snippet.thumbnails.standard.url).into(
+            thumbnailImageView
+        )
 
         // display video details: views, published time, likes, dislikes
-        val viewCount = oneVideo.statistics.viewCount
-        val publishedAt = oneVideo.snippet.publishedAt
-        holder.view.viewCount_publishedAt_TV.text = viewCount + publishedAt
-        holder.view.likeCountTV.text = oneVideo.statistics.likeCount
-        holder.view.dislikeCountTV.text = oneVideo.statistics.dislikeCount
+        val viewCount = getThousands(oneVideo.statistics.viewCount)
+        val publishedAt = getTimeAgo(stringToDate(oneVideo.snippet.publishedAt.substringBefore('T')))
+        holder.view.viewCount_publishedAt_TV.text = "$viewCount views • $publishedAt"
+        holder.view.likeCountTV.text = getShortScale(oneVideo.statistics.likeCount)
+        holder.view.dislikeCountTV.text = getShortScale(oneVideo.statistics.dislikeCount)
+    }
+
+    // convert Integer to String with comma for thousands
+    private fun getThousands(count: String): String {
+        return NumberFormat.getNumberInstance(Locale.US)
+            .format(count.toInt())
+    }
+
+    // convert String to Date
+    private fun stringToDate(date: String): Date {
+        val defaultZoneId: ZoneId = ZoneId.systemDefault()
+        val localDate = LocalDate.parse(date, DateTimeFormatter.ISO_DATE)
+        return Date.from(localDate.atStartOfDay(defaultZoneId).toInstant())
+    }
+
+    // count occurrences of a given character in a string
+    private fun countOccurrences(s: String, c: Char): Int {
+        return s.filter{ it == c }.count()
+    }
+
+    // convert String number in billions, millions, thousands, e.g., 317,000 -> 317K
+    private fun getShortScale(number: String): String {
+        val numInThousands = getThousands(number)
+        val leadingNum = numInThousands.substringBefore(',')
+        val numCommas = countOccurrences(numInThousands, ',')
+        return leadingNum + when (numCommas) {
+            0 -> ""
+            1 -> "K" // thousand
+            2 -> "M" // million
+            else -> "B" // billion
+        }
+    }
+
+    // convert published date to ... time ago
+    // * Source: https://stackoverflow.com/questions/35858608/how-to-convert-time-to-time-ago-in-android
+    private fun currentDate(): Date {
+        val calendar = Calendar.getInstance()
+        return calendar.time
+    }
+
+    private fun getTimeAgo(date: Date): String {
+        var time = date.time
+        if (time < 1000000000000L) {
+            time *= 1000
+        }
+
+        val now = currentDate().time
+        if (time > now || time <= 0) {
+            return "in the future"
+        }
+
+        val diff = now - time
+        return when {
+            diff < MINUTE_MILLIS -> "moments ago"
+            diff < 2 * MINUTE_MILLIS -> "a minute ago"
+            diff < 60 * MINUTE_MILLIS -> "${diff / MINUTE_MILLIS} minutes ago"
+            diff < 2 * HOUR_MILLIS -> "an hour ago"
+            diff < 24 * HOUR_MILLIS -> "${diff / HOUR_MILLIS} hours ago"
+            diff < 48 * HOUR_MILLIS -> "yesterday"
+            else -> "${diff / DAY_MILLIS} days ago"
+        }
     }
 }
 
